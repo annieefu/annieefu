48e3c89a9ba3992bfbc5128e11589df8b2ac3ea0
Annie Fu <af397@cornell.edu>
2020-01-17 12:46:38 -0500

    Import
    All files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// DO NOT REMOVE!
+include("includes/init.php");
+// // DO NOT REMOVE!
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+  <script type="text/javascript" src="scripts/jquery-3.4.1.min.js"></script>
+<link rel="stylesheet" href="https://use.typekit.net/opt0ipj.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.typekit.net/opt0ipj.css">
+  <title>Annie Fu</title>
+</head>
+<body class="home">
+<?php include("includes/header.php"); ?>
+
+</body>
+</html>
