6c4c7fa8f4219083cbe6345c442617294b1ac3cb
Annie Fu <af397@cornell.edu>
2020-02-07 00:14:23 -0500

    PLEASE

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,8 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-<meta name="viewport" content="initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <meta name="viewport" content="initial-scale=1"> -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
   <script type="text/javascript" src="scripts/jquery-3.4.1.min.js"></script>
