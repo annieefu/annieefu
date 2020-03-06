30457a08816194ec7998c4ada2690ef633805775
Annie Fu <af397@cornell.edu>
2020-03-06 01:46:39 -0500

    res

--- resume.php
+++ resume.php
@@ -17,7 +17,7 @@ $about="notcurrentpage";
 
 <div class="col-12">
 
-<object data="annie-fu-resume.pdf" type="application/pdf" width="100%" height="600px"> 
+<object data="AnnieFu-Res2020.pdf" type="application/pdf" width="100%" height="600px"> 
   <p>It appears you don't have a PDF plugin for this browser.
    No biggie... you can <a href="AnnieFu-Res2020.pdf">click here to
   download the PDF file.</a></p>  
