2c5b82d5136ed440ddf9d618c7d102eda863d209
Annie Fu <af397@cornell.edu>
2020-04-22 17:11:41 -0400

    res

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
@@ -21,6 +22,7 @@
 <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
 
 <title>Annie Fu</title>
+
 
 </head>
   
--- resume.php
+++ resume.php
@@ -17,7 +17,7 @@ $art = "notcurrentpage";
 
 <div class="col-12">
 
-<object data="AnnieFu_Resume_Su20.pdf" type="application/pdf" width="100%" height="600px"> 
+<object data="Annie_Fu_Resume2020.pdf" type="application/pdf" width="100%" height="600px"> 
   <p>It appears you don't have a PDF plugin for this browser.
    No biggie... you can <a href="Annie_Fu_Resume_S20.pdf" target="_blank">click here to
   download the PDF file.</a></p>  
