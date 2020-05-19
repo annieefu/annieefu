eab17bd408caace3183d163d3365f0840f0f9044
Annie Fu <af397@cornell.edu>
2020-05-18 21:20:30 -0400

    small wording change

--- resume.php
+++ resume.php
@@ -19,7 +19,7 @@ $art = "notcurrentpage";
 
 <object data="AnnieFu_Resume.pdf" type="application/pdf" width="100%" height="1000px"> 
   <p>It appears you don't have a PDF plugin for this browser.
-   No biggie... you can <a href="AnnieFu_Resume.pdf" target="_blank">click here to
+   No problem! You can <a href="AnnieFu_Resume.pdf" target="_blank">click here to
   download the PDF file.</a></p>  
 </object>
 
